6380453449ce9248b67c4b0f908e39408e1aef77
jinhkk <kwlsgur00@naver.com>
2025-05-16 21:23:22 +0900

    tmp

--- practice/my_linear.py
+++ practice/my_linear.py
@@ -48,7 +48,7 @@ model = Sequential()
 model.add(Dense(1, input_dim=1, activation='linear'))
 
 model.compile(optimizer=SGD(learning_rate=1e-6), loss='mse')
-model.fit(x.numpy(), y.numpy(), epochs=2000) # TensorFlow 모델은 TensorFlow 텐서 또는 NumPy 배열이 필요하다
+model.fit(x.numpy(), y.numpy(), epochs=20000) # TensorFlow 모델은 TensorFlow 텐서 또는 NumPy 배열이 필요하다
 
 plt.scatter(x, y)
 plt.plot(x, model.predict(x),'r')
